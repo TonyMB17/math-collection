6bbdb16c25f76c8ea3435dd679bd2f43fad6a2ef
TonyMB17 <anthonyamb17@gmail.com>
2023-06-27 23:47:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'math collection',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
