3a6348720224c356341769b9d6e2a52f93c98cc0
xXEDNYXx <191200@unamba.edu.pe>
2023-07-26 01:17:55 -0500

    formulas v3

--- src/app/pages/estadistica/estadistica.page.ts
+++ src/app/pages/estadistica/estadistica.page.ts
@@ -10,43 +10,239 @@ import { ModalComponent } from 'src/app/components/modal/modal.component';
 export class EstadisticaPage implements OnInit {
 
   public formulas = [
-    { 
-      title: 'Nombre del tema 1', 
+    { //¿Qué es la Estadística?
+      title: '¿Qué es la Estadística?', 
       data: [
         { 
-          title: 'Nombre de la formula 1.1', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Estadística', 
+          src: "assets/estadistica/estadistica 30.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 1.2', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Clases de Estadística', 
+          src: "assets/estadistica/estadistica 31.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 1.3', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Población y Muestra', 
+          src: "assets/estadistica/estadistica 32.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Variables Estadísticas', 
+          src: "assets/estadistica/estadistica 33.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Presentación de los datos mediante tablas o cuadros', 
+          src: "assets/estadistica/estadistica 34.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Tamaño de la muestra (n):', 
+          src: "assets/estadistica/estadistica 35.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Alcance (A):', 
+          src: "assets/estadistica/estadistica 36.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Rango (R):', 
+          src: "assets/estadistica/estadistica 37.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Frecuencia Absoluta (fi):', 
+          src: "assets/estadistica/estadistica 38.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Mediana (Med):', 
+          src: "assets/estadistica/estadistica 39.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Mediana para Datos Agrupados por Intervalos de Clase', 
+          src: "assets/estadistica/estadistica 40.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Moda (Z ó mod):', 
+          src: "assets/estadistica/estadistica 41.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Moda para Datos Agrupados por Intervalos de Clase', 
+          src: "assets/estadistica/estadistica 42.jpg", 
+          description: ""
+        },
+
+      ] 
+    },
+    { //Distribuciones de Frecuencias
+      title: 'Distribuciones de Frecuencias', 
+      data: [
+        { 
+          title: 'Frecuencia Relativa', 
+          src: "assets/estadistica/estadistica 1.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Medidas de Tendencia Central', 
+          src: "assets/estadistica/estadistica 2.jpg", 
+          description: "ni → frecuencias absoluta; N = n1 + n2 + n3 + … + ni;	N → Número de casos de la población"
         },
       ] 
     },
-    { 
-      title: 'Nombre del tema 2', 
+    { //Medidas de Tendencia Central
+      title: 'Medidas de Tendencia Central', 
       data: [
         { 
-          title: 'Nombre de la formula 2.1', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Media Aritmética', 
+          src: "assets/estadistica/estadistica 3.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 2.2', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Media Aritmética datos agrupados', 
+          src: "assets/estadistica/estadistica 4.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 2.3', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Media estadística Ponderada', 
+          src: "assets/estadistica/estadistica 5.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Cálculo de las Marcas de Clase', 
+          src: "assets/estadistica/estadistica 6.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Mediana', 
+          src: "assets/estadistica/estadistica 7.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Cuartiles', 
+          src: "assets/estadistica/estadistica 8.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Deciles', 
+          src: "assets/estadistica/estadistica 9.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Centiles', 
+          src: "assets/estadistica/estadistica 10.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Moda', 
+          src: "assets/estadistica/estadistica 11.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Cálculo de Percentiles', 
+          src: "assets/estadistica/estadistica 12.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Rango Intercuántico', 
+          src: "assets/estadistica/estadistica 13.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Rango Semi-Intercuantílico', 
+          src: "assets/estadistica/estadistica 14.jpg", 
+          description: ""
+        },
+      ] 
+    },
+    { //Medidas de Dispersión
+      title: 'Medidas de Dispersión', 
+      data: [
+        { 
+          title: 'Varianza', 
+          src: "assets/estadistica/estadistica 15.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Desviación Estándar (o Típica)', 
+          src: "assets/estadistica/estadistica 16.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Varianza para datos agrupados', 
+          src: "assets/estadistica/estadistica 17.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Desviación Estándar (o Típica) para datos agrupados', 
+          src: "assets/estadistica/estadistica 18.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Coeficiente de Variación ', 
+          src: "assets/estadistica/estadistica 19.jpg", 
+          description: "para comparar el grado de dispersión entre varias distribuciones"
+        },
+      ] 
+    },
+    { //Teoría y cálculo de Probabilidades
+      title: 'Teoría y cálculo de Probabilidades', 
+      data: [
+        { 
+          title: 'Probabilidad a priori y frecuencia relativa', 
+          src: "assets/estadistica/estadistica 20.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Suceso Complementario', 
+          src: "assets/estadistica/estadistica 21.jpg", 
+          description: ""
+        },
+        { 
+          title: '', 
+          src: "assets/estadistica/estadistica 22.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Permutación', 
+          src: "assets/estadistica/estadistica 23.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Variaciones', 
+          src: "assets/estadistica/estadistica 24.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Combinaciones', 
+          src: "assets/estadistica/estadistica 25.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Tipos de Sucesos', 
+          src: "assets/estadistica/estadistica 26.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Regla de la Adicción: Unión de Sucesos (simultáneos)', 
+          src: "assets/estadistica/estadistica 27.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Regla de la Multiplicación: Sucesos Condicionados o Intersección de Sucesos (secuenciales)', 
+          src: "assets/estadistica/estadistica 28.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Regla de Laplace', 
+          src: "assets/estadistica/estadistica 29.jpg", 
+          description: "La probabilidad de un suceso es igual a su cardinal entre el cardinal del espacio muestral en el que se inscribe"
         },
       ] 
     },
--- src/app/pages/trigonometria/trigonometria.page.ts
+++ src/app/pages/trigonometria/trigonometria.page.ts
@@ -10,43 +10,308 @@ import { ModalComponent } from 'src/app/components/modal/modal.component';
 export class TrigonometriaPage implements OnInit {
 
   public formulas = [
-    { 
-      title: 'Nombre del tema 1', 
+    { //ÁNGULO TRIGONOMÉTRICO Y LONGITUD DE ARCO
+      title: 'ÁNGULO TRIGONOMÉTRICO Y LONGITUD DE ARCO', 
       data: [
         { 
-          title: 'Nombre de la formula 1.1', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Fórmulas de Conversión:', 
+          src: "assets/trigonometria/trigonometria 1.jpg", 
+          description: "Se observa que el cociente de su medida con la medida del ángulo de una vuelta es una constante."
         },
         { 
-          title: 'Nombre de la formula 1.2', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: '', 
+          src: "assets/trigonometria/trigonometria 2.jpg", 
+          description: "Aprovechando esta característica, ahora tomaremos un ángulo cualquiera:"
         },
         { 
-          title: 'Nombre de la formula 1.3', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Longitud de Arco en un Sector Circular', 
+          src: "assets/trigonometria/trigonometria 3.jpg", 
+          description: "Un sector circular es una parte de un círculo donde un vértice es el centro del círculo y los otros dos son puntos de la circunferencia. Entre sus tres elementos se cumple las siguientes relaciones:"
+        },
+        { 
+          title: 'Superficie de un Sector Circular', 
+          src: "assets/trigonometria/trigonometria 4.jpg", 
+          description: ""
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/trigonometria 5.jpg", 
+          description: "Si interceptamos dos sectores circulares, se forma una figura que por su semejanza con el trapecio recibe en nombre de «Trapecio Circular«. La superficie de esta figura se puede calcular del siguiente modo:"
+        },
+        { 
+          title: 'Poleas en Contacto', 
+          src: "assets/trigonometria/trigonometria 6.jpg", 
+          description: "longitudes recorridas iguales"
+        },
+        { 
+          title: 'Poleas unidas por una faja transportadora', 
+          src: "assets/trigonometria/trigonometria 7.jpg", 
+          description: "Longitudes recorridas iguales"
+        },
+        { 
+          title: 'Poleas unidas a través de un eje', 
+          src: "assets/trigonometria/trigonometria 8.jpg", 
+          description: "ángulos girados iguales"
+        },
+        { 
+          title: 'Rueda en un plano horizontal', 
+          src: "assets/trigonometria/trigonometria 9.jpg", 
+          description: ""
         },
       ] 
     },
-    { 
-      title: 'Nombre del tema 2', 
+    { //RAZONES TRIGONOMÉTRICAS DE UN ÁNGULO AGUDO
+      title: 'RAZONES TRIGONOMÉTRICAS DE UN ÁNGULO AGUDO', 
       data: [
         { 
-          title: 'Nombre de la formula 2.1', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Razón Trigonométrica de un Ángulo Agudo', 
+          src: "assets/trigonometria/Razon 1.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 2.2', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: '', 
+          src: "assets/trigonometria/Razon 2.jpg", 
+          description: ""
         },
         { 
-          title: 'Nombre de la formula 2.3', 
-          src: "https://economipedia.com/wp-content/uploads/image-463.png", 
-          description: "descripcion de la fornula 1"
+          title: 'Razones Trigonométricas Recíprocas', 
+          src: "assets/trigonometria/Razon 3.jpg", 
+          description: "Las razones recíprocas son aquellas cuyo valor es el inverso aritmético de la otra, por ejemplo:  y  son razones que cumplen esta condición. En las razones trigonométricas encontramos algunas parejas de razones recíprocas, a saber:"
+        },
+        { 
+          title: 'El producto de dos razones recíprocas es siempre la unidad', 
+          src: "assets/trigonometria/Razon 4.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Razones Trigonométricas de Ángulos Complementarios', 
+          src: "assets/trigonometria/Razon 5.jpg", 
+          description: "Al definir las razones trigonométricas, hemos considerado a uno de los ángulos agudos, pero también se puede tomar las razones trigonométricas del otro ángulo o sea de su complemento y como se trata de los mismos lados encontramos algunas igualdades:"
+        },
+        { 
+          title: 'Propiedad Fundamental de las Razones Trigonométricas', 
+          src: "assets/trigonometria/Razon 6.jpg", 
+          description: "A continuación vamos a establecer de que dependen las razones trigonométricas. Sea APO = θ  un ángulo agudo, en la recta OP tome los puntos cualesquiera B y D trácense BC y DE perpendiculares a OA. Tome también un punto F en OP y trácese la perpendicular FG."
+        },
+        { 
+          title: 'Razones trigonométricas de 30º y 60º', 
+          src: "assets/trigonometria/Razon 7.jpg", 
+          description: "Construimos un triángulo equilátero, en el cual trazamos la altura y formamos dos triángulos rectángulos de 30º y 60º. Si uno de los lados del triángulo equilátero es «L», los lados del triángulo rectángulo son L/2 y L√3/2 , de donde deducimos la propiedad:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Razon 8.jpg", 
+          description: "Pero para tomar el valor de las razones trigonométricas de 30º y 60º, es más cómodo dar un valor a «L», pues las razones no dependen de la longitud de los lados. Así para , tenemos:"
+        },
+        { 
+          title: 'Razones trigonométricas de 45º', 
+          src: "assets/trigonometria/Razon 9.jpg", 
+          description: "Construimos un triángulo rectángulo isósceles y a uno de los catetos le llamamos «L», luego la  hipotenusa se calcula por Pitágoras y su valor es «L√2». Para obtener las razones trigonométricas de 45º, hacemos L=1 y tenemos:"
+        },
+        { 
+          title: 'Razones Trigonométricas de 37º y 53º', 
+          src: "assets/trigonometria/Razon 10.jpg", 
+          description: ""
+        },
+      ] 
+    },
+    { //RAZONES TRIGONOMÉTRICAS DE ÁNGULOS DE CUALQUIER MAGNITUD
+      title: 'RAZONES TRIGONOMÉTRICAS DE ÁNGULOS DE CUALQUIER MAGNITUD', 
+      data: [
+        { 
+          title: 'Sistema de Coordenadas Rectangulares.', 
+          src: "assets/trigonometria/coordenadas 1.jpg", 
+          description: "Es el sistema que consta de dos rectas dirigidas (rectas numéricas) las cuales se cortan perpendicularmente en el número cero. Estas dos rectas numéricas se denominan ejes coordenados."
+        },
+        { 
+          title: 'Punto medio de un segmento', 
+          src: "assets/trigonometria/coordenadas 2.jpg", 
+          description: "El punto medio de un segmento del cual se conocen las coordenadas de sus extremos se calcula de la siguiente manera:"
+        },
+        { 
+          title: 'Distancia entre dos puntos', 
+          src: "assets/trigonometria/coordenadas 3.jpg", 
+          description: "La distancia entre dos puntos A y B se calcula se la siguiente manera:"
+        },
+        { 
+          title: 'Cálculo del radio vector (R)', 
+          src: "assets/trigonometria/coordenadas 4.jpg", 
+          description: "La distancia de un punto P(x;y) al origen de coordenadas se denomina radio vector el cual siempre será positivo y se calcula de la siguiente manera:"
+        },
+        { 
+          title: 'Ángulos en Posición Normal', 
+          src: "assets/trigonometria/coordenadas 5.jpg", 
+          description: ""
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/coordenadas 6.jpg", 
+          description: "Se denominan ángulos en posición normal, a alguien cuyo lado inicial coincide con el semieje positivo de las abscisas, su vértice con el origen de coordenadas y su lado final se encuentra en cualquier parte del plano (cuadrantes o sobre los ejes coordenados). A estos ángulos también se les denomina ángulos en posición estándar. A continuación se muestra algunos ejemplos de ángulos en posición normal."
+        },
+        { 
+          title: 'Ángulos Cuadrantales', 
+          src: "assets/trigonometria/coordenadas 7.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Los principales ángulos cuadrantales son:', 
+          src: "assets/trigonometria/coordenadas 8.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Razones Trigonométricas de los Ángulos en Posición Normal', 
+          src: "assets/trigonometria/coordenadas 9.jpg", 
+          description: ""
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/coordenadas 10.jpg", 
+          description: "Se definen las razones trigonométricas de «α» de la siguiente manera:"
+        },
+        { 
+          title: 'Signos de las Razones Trigonométricas', 
+          src: "assets/trigonometria/coordenadas 11.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Signos de las Razones Trigonométricas', 
+          src: "assets/trigonometria/coordenadas 12.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Razones Trigonométricas de los Ángulos Cuadrantales', 
+          src: "assets/trigonometria/coordenadas 13.jpg", 
+          description: ""
+        },
+      ] 
+    },
+    { //IDENTIDADES TRIGONOMÉTRICAS
+      title: 'IDENTIDADES TRIGONOMÉTRICAS', 
+      data: [
+        { 
+          title: 'Identidades Reciprocas:', 
+          src: "assets/trigonometria/Identidades 1.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Identidades Reciprocas:', 
+          src: "assets/trigonometria/Identidades 2.jpg", 
+          description: ""
+        },
+        { 
+          title: 'Identidades por Cociente:', 
+          src: "assets/trigonometria/Identidades 3.jpg", 
+          description: "Utilizando el gráfico anterior:"
+        },
+        { 
+          title: 'Identidades Pitagóricas:', 
+          src: "assets/trigonometria/Identidades 4.jpg", 
+          description: "Aplicando el teorema de PITÁGORAS en la figura anterior, se obtiene:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Identidades 5.jpg", 
+          description: "Dividiendo entre: Cos² x la identidad (6), obtenemos:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Identidades 6.jpg", 
+          description: "usando las identidades (4) y (2):"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Identidades 7.jpg", 
+          description: "Del mismo modo, usando las identidades (6), (5) y (1)"
+        },
+        { 
+          title: 'Identidades Trigonométricas Auxiliares', 
+          src: "assets/trigonometria/Identidades 8.jpg", 
+          description: "Las identidades trigonométricas auxiliares más importantes son:"
+        },
+        { 
+          title: 'Demostraciones de Identidades Trigonométricas', 
+          src: "assets/trigonometria/Identidades 9.jpg", 
+          description: "Para demostrar identidades trigonométricas no hay una técnica ni un procedimiento especial. Las siguientes sugerencias ayudan a realizar dichas demostraciones."
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Identidades 10.jpg", 
+          description: "De las identidades trigonométricas fundamentales se pueden deducir otras así:"
+        },
+      ] 
+    },
+    { //TRANSFORMACIONES TRIGONOMÉTRICAS
+      title: 'TRANSFORMACIONES TRIGONOMÉTRICAS', 
+      data: [
+        { 
+          title: 'Transformación de una Suma y Diferencia de Senos en un Producto', 
+          src: "assets/trigonometria/Transformacion 1.jpg", 
+          description: "Nuevamente debemos indicar un error frecuente cuando se tiene que transformar a producto."
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 1.jpg", 
+          description: ""
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 2.jpg", 
+          description: "Para factorizar una suma o diferencia de senos, se recurrirá a las fórmulas de la suma y diferencia de ángulos."
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 3.jpg", 
+          description: "Sumando y restando (1) y (2), tenemos:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 4.jpg", 
+          description: "Haciendo un cambio de variable:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 5.jpg", 
+          description: "Luego reemplazando tenemos:"
+        },
+        { 
+          title: 'Transformación de Suma y Diferencia a de Cosenos en un Producto', 
+          src: "assets/trigonometria/Transformacion 6.jpg", 
+          description: "Al igual que las fórmulas anteriores emplearemos las relaciones de la suma y diferencia de ángulos:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 7.jpg", 
+          description: "Sumando y restando (3) y (4) tenemos:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 8.jpg", 
+          description: "Haciendo un cambio de variable:"
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 9.jpg", 
+          description: "Reemplazando tenemos:"
+        },
+        { 
+          title: 'Casos Especiales de Factorización Trigonométrica', 
+          src: "assets/trigonometria/Transformacion 10.jpg", 
+          description: "Además de factorizar la suma o diferencia de senos y cosenos, podemos encontrar otras expresiones que se pueden transformar a producto aplicando las relaciones anteriores. Analicemos algunos de ellos:"
+        },
+        { 
+          title: 'Transformación de un Producto de Senos y Cosenos en una Suma o Diferencia', 
+          src: "assets/trigonometria/Transformacion 11.jpg", 
+          description: "Para transformar un producto de senos y cosenos en una suma o diferencia, tenemos que recurrir a las fórmulas de seno y coseno de la suma y diferencia de ángulos."
+        },
+        { 
+          title: '', 
+          src: "assets/trigonometria/Transformacion 12.jpg", 
+          description: "Sumando (1) y (2)"
+        },
+        { 
+          title: 'Propiedades', 
+          src: "assets/trigonometria/Transformacion 13.jpg", 
+          description: "Existen muchas relaciones trigonométricas que nos permiten pasar de un producto de funciones a suma o diferencia, pero, las más notables son:"
         },
       ] 
     },
